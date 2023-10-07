ebcdcaaecdb4c9b4470551137fcc3109743fb0ec
User <gorbatovskiy.igor@gmail.com>
2023-10-07 18:48:33 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  bracketsConfigMap = new Map();
+  bracketsConfig.forEach(([key, value]) => bracketsConfigMap.set(value, key));
+  str = str.split("");
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (Array.from(bracketsConfigMap.values()).includes(str[i])) {
+      bracketsConfigMap.get(str[i]) === str[i] && stack.includes(str[i])
+        ? stack.pop()
+        : stack.push(str[i]);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      let lastBracket = stack[stack.length - 1];
+      if (bracketsConfigMap.get(str[i]) === lastBracket) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
